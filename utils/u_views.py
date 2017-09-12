957091a1f51d45f37360ad18e0219f65ee4ac201
DickJ <hcir614@gmail.com>
2017-09-12 11:19:20 -0500

    Cleaned up code

--- utils/u_views.py
+++ utils/u_views.py
@@ -1,11 +1,8 @@
+from app import app
+from flask import flash
 import logging
 import random
 import re
-
-import psycopg2
-from flask import flash
-
-from app import app
 from utils import u_db
 from utils.classes.TextClient import TextClient
 
@@ -14,37 +11,32 @@ def run_signup_form(form):
     # Open db connection
     conn, cur = u_db.get_db_conn_and_cursor(app.config)
 
+    # FIXME: What do we do if user selects invalid phone provider?
+    # FIXME: How will this fix play into the eventual switch to TWILIO?
     # If all data is valid
     phone = is_valid_number(form.phone.data)
     if phone:
         # If phone number does not already exist as a verified user
         cur.execute('SELECT * FROM verified WHERE phone=%s;', [phone])
         if not cur.fetchone():
-            logging.info({'func': 'index', 'fname': form.fname.data,
+            logging.info({'func': 'run_signup_form', 'fname': form.fname.data,
                           'lname': form.lname.data, 'phone': phone,
                           'provider': form.provider.data,
                           'msg': 'user signup'})
             # If phone number does not already exist as an unverified user
-            cur.execute("SELECT * FROM unverified WHERE phone=%s;", [phone])
-            if not cur.fetchone():
-                # Add user to unverified signups table
-                confcode = random.getrandbits(16)
-                flashmsg = sign_up_user(cur, phone, form.provider.data,
+            cur.execute('SELECT confcode FROM unverified WHERE phone=%s;', [phone])
+            unverified_user = cur.fetchone()
+            if not unverified_user:
+                #  Add user to unverified signups table
+                flashmsg = sign_up_user(cur, conn, phone, form.provider.data,
                                         form.lname.data.upper(),
-                                        form.fname.data.upper(), confcode)
+                                        form.fname.data.upper())
 
-                # Send Text Message via SendBox
-                subject = 'VT-3 Notifications'
-                smstxt = 'Click the link to confirm. %s%s%d' \
-                         % (app.config['BASE_URL'], '/verify/', confcode)
-                client = TextClient(debug=app.config['DEBUG'])
-                response = client.send_message(phone, form.provider.data,
-                                               subject, smstxt)
-
-
-            # TODO Should I resend a new confirmation code here?
             else:
-                flashmsg = 'This phone number has already signed up but has not been verified. Please check your phone for your confirmation code.'
+                confcode = int(unverified_user[0])
+                send_conf_code(phone, form.provider.data,
+                               'VT-3 Notifications Signup', confcode)
+                flashmsg = 'This phone number has already signed up but has not been verified. We have re-sent your confirmation code.'
         else:
             flashmsg = 'This phone number has already been signed up.'
 
@@ -54,9 +46,6 @@ def run_signup_form(form):
         flash('Invalid phone number. Please try again.')
         form.phone.errors.append("Invalid format.")
 
-    # TODO Should the commit be moved into helpers.sign_up_user()?
-    # -> would need to pass conn
-    conn.commit()
     cur.close()
     conn.close()
 
@@ -97,54 +86,59 @@ def is_valid_number(number):
     else:
         return False
 
-def get_db_conn_and_cursor(config):
-    """
-    Obtains a connection and cursor object to the PostgresSQL database
-
-    Args:
-        config: (dict) Contains configuration variables
-
-    Returns: psycopg2.extensions.connection, psycopg2.extensions.cursor
-    """
-    conn = psycopg2.connect(
-        database=config['PG_URL'].path[1:],
-        user=config['PG_URL'].username,
-        password=config['PG_URL'].password,
-        host=config['PG_URL'].hostname,
-        port=config['PG_URL'].port
-    )
-    cur = conn.cursor()
-
-    return conn, cur
-
-def sign_up_user(cur, phone, provider, lname, fname, confcode):
+def sign_up_user(cur, conn, phone, provider, lname, fname):
     """
     Adds a user to the unverified users table
 
     Params:
         cur: (psycopg2.extensions.cursor) A cursor to the database
+        conn: (psycopg2.extensions.connection) A connecton to the database
         phone: (str) phone number in E.164 format
         provider: (str) a wireless provider
         lname: (str) user's last name
         fname: (str) user's first name
-        confcode: (str) random 6-digit code
 
     Returns
         (str) a response message to the user to be flashed on the webpage
     """
-    assert re.match('\+\d{9}', phone), "Invalid phone number format: %r" % phone
-    assert type(confcode) == str and len(confcode) == 6, "Invalid confirmation code: %r" % confcode
+    assert re.match('^\+1\d{10}$', phone), "Invalid phone number format: %r" % phone
     assert type(fname == str), 'Invalid data type for first name: %r' % type(fname)
     assert type(lname == str), 'Invalid data type for last name: %r' % type(lname)
     assert fname != '', 'Last name is blank.'
     assert lname != '', 'First name is blank.'
+
+    confcode = random.getrandbits(16)
 
     cur.execute(
         "INSERT INTO unverified (phone, provider, lname, fname, confcode, datetime) VALUES (%s, %s, %s, %s, %s, current_timestamp);",
         [phone, provider, lname, fname, confcode]
     )
 
+    send_conf_code(phone, provider, 'VT-3 Notifications Signup', confcode)
+
+    conn.commit()
+
     return 'Signup requested for %s. You should receive a text message shortly' % (phone)
+
+def send_conf_code(phone, provider, subject, confcode):
+    """
+    Send a confirmation code to the user.
+
+    #TODO In the future, this code could probably be changed so that it can be
+    # the function used by worker to send out the schedule as well. This would
+    # make converting to Twilio down the road easier, as there will only be one
+    # function to change.
+
+    :param phone: (str) phone number in E.164 format
+    :param provider: (str) a wireless provider
+    :param subject: (str) message subject (basically subscribe or unsubscribe)
+    :param confcode: 16 bit confirmation code
+    :return: the sendgrid response
+    """
+    smstxt = 'Click the link to confirm %s%s%d' \
+             % (app.config['BASE_URL'], '/verify/', confcode)
+    client = TextClient(debug=app.config['DEBUG'])
+    return client.send_message(phone, provider, subject, smstxt)
 
 def unsubscribe_user(cur, phone, confcode):
     """
@@ -164,8 +158,3 @@ def unsubscribe_user(cur, phone, confcode):
     cur.execute("INSERT INTO unsubscribe (phone, confcode) VALUES (%s, %s);", [phone, confcode])
 
     return 'Unsubscribe requested for %s. You should receive a text message shortly to confirm unsubscribe' % (phone)
-
-def welcome_message(phone, name):
-    """
-    """
-    pass
