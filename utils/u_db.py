c29f8d88cb5b0b7572ce13df61e23b51d2050f74
DickJ <hcir614@gmail.com>
2017-09-15 15:57:04 -0500

    updated db connection helper function

--- utils/u_db.py
+++ utils/u_db.py
@@ -1,6 +1,9 @@
+import os
+from urllib import parse
 import psycopg2
 
-def get_db_conn_and_cursor(config):
+
+def get_db_conn_and_cursor(config=None):
     """
     Obtains a connection and cursor object to the PostgresSQL database
 
@@ -9,6 +12,9 @@ def get_db_conn_and_cursor(config):
 
     Returns: psycopg2.extensions.connection, psycopg2.extensions.cursor
     """
+    if config == None:
+        parse.uses_netloc.append('postgres')
+        config = {'PG_URL': parse.urlparse(os.environ["DATABASE_URL"])}
     conn = psycopg2.connect(
         database=config['PG_URL'].path[1:],
         user=config['PG_URL'].username,
