1326e1b780ae4251a7a188bf2423c8dc0a7f67de
Rich <richard.jerome.johnson@gmail.com>
2017-10-09 09:22:59 -0500

    Updated TextClient to support Twilio and added unittests

--- utils/TextClient.py
+++ utils/TextClient.py
@@ -4,6 +4,7 @@ import re
 
 import sendgrid
 from sendgrid.helpers.mail import *
+from twilio.rest import Client
 
 from utils import u_db
 
@@ -13,37 +14,51 @@ class TextClient:
         self.debug = debug
         self.msg_svc = msg_svc
 
-        if debug:
-            self.msgr = None
+        self.twilio_client = Client(os.environ.get('TWILIO_SID'),
+                                    os.environ.get('TWILIO_AUTH_TOKEN'))
+        self.phone = os.environ.get('TWILIO_PHONE_NUMBER')
 
-        if msg_svc == 'sendgrid':
-            self.msgr = sendgrid.SendGridAPIClient(
-                apikey=os.environ.get('SENDGRID_API_KEY'))
-            self.from_email = Email('vt3@herokuapp.com')
-        elif msg_svc == 'twilio':
-            pass
-        else:
-            pass
+        self.sendgrid_client = sendgrid.SendGridAPIClient(
+            apikey=os.environ.get('SENDGRID_API_KEY'))
+        self.from_email = Email(os.environ.get('SENDGRID_FROM_EMAIL'))
 
-    def send_message(self, phone, provider, date, msg):
-        assert self.msg_svc == 'sendgrid', \
-            "Attempting to use sendgrid with twilio set as message service."
+    def send_message(self, phone, date, msg, provider=None):
+        """
+        Send a message via SMS
+        :param phone: (str) Phonen umber in E.164 format
+        :param date: (str) MMM DD
+        :param msg: (str) the message to send
+        :param provider: (str) Wireless provider; needed for use with sendgrid
+        :return: message response
+        """
         assert phone[0] == '+' and phone[1] == '1', \
             'TextClient:send_message() - Invalid phone number %r' % phone
 
-        phone = phone[2:]
-        subject = date
-        to_email = Email(self.from_email_address(phone, provider))
-        content = Content("text/plain", msg)
-        mail = Mail(self.from_email, subject, to_email, content)
+        if self.msg_svc == 'sendgrid':
+            assert provider is not None, 'No wireless provider given'
+            phone = phone[2:]
+            subject = date
+            to_email = Email(self.from_email_address(phone, provider))
+            content = Content("text/plain", msg)
+            mail = Mail(self.from_email, subject, to_email, content)
 
-        if self.debug:
-            response = str(mail.get())
-            logging.debug(response)
-        else:
-            response = self.msgr.client.mail.send.post(request_body=mail.get())
-
+            if self.debug:
+                response = str(mail.get())
+                logging.debug(response)
+            else:
+                response = self.sendgrid_client.client.mail.send.post(
+                    request_body=mail.get())
+        elif self.msg_svc == 'twilio':
+            message = '({}) {}'.format(date, msg)
+            if self.debug:
+                logging.debug('{} {}'.format(phone, message))
+                response = 0
+            else:
+                message = self.twilio_client.messages.create(
+                    to=phone, from_=self.phone, body=message)
+                response = message.sid
         return response
+
 
     @staticmethod
     def from_email_address(phone, provider):
@@ -59,13 +74,19 @@ class TextClient:
 
         if phone[:2] == '+1':
             phone = phone[2:]
-        assert len(phone) == 10
-        assert re.match('\d{10}', phone)
+        assert len(phone) == 10, 'phone is not lenght 10'
+        assert re.match('\d{10}', phone), 'phone is not all digits'
+        assert provider is not None, 'No wireless provider given'
 
         conn, cur = u_db.get_db_conn_and_cursor()
         cur.execute('SELECT gateway FROM smsgateways WHERE name = %s;', [provider])
 
-        # Is this really the best behavior I can come up with?
+        #TODO Is this really the best behavior I can come up with?
+        # A better solution might be to force usage with Twilio, however if
+        # Twilio is setup, then why don't I just use twilio for everything in
+        # the first place? For now, I am simply going to leave this as the state
+        # of things until I complete the implementation of Twilio. At that point
+        # This entire function should be removed.
         try:
             gateway_address = cur.fetchone()[0]
         except TypeError:
